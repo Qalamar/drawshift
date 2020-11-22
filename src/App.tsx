733e5563f5ab10c2e082eb7e89a6a4506c6856c2
Qalamar <contact@tariqhamrit.com>
2020-11-22 09:01:26 +0100

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       {/* // Gwt */}
-      <div className="relative bg-deep-purple-accent-400">
+      <div className="relative bg-deep-purple-accent-400 h-auto">
         <div className="absolute inset-x-0 bottom-0">
           <svg
             viewBox="0 0 224 12"
@@ -17,21 +17,21 @@ const App = () => {
             <path d="M0,0 C48.8902582,6.27314026 86.2235915,9.40971039 112,9.40971039 C137.776408,9.40971039 175.109742,6.27314026 224,0 L224,12.0441132 L0,12.0441132 L0,0 Z" />
           </svg>
         </div>
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-40">
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 md:py-64">
           <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
-            <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-              Games are our passion
-              <br className="hidden md:block" />
-              We make games for{" "}
+            <h1 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
               <span className="relative inline-block px-2">
-                <div className="absolute inset-0 transform -skew-x-12 bg-teal-accent-400" />
-                <span className="relative text-teal-900">lazy dog</span>
+                <span className="relative text-teal-accent-400">Drawshift</span>
+                <div className="w-full h-3 -mt-3 bg-deep-purple-accent-200" />
               </span>
-            </h2>
+              is the game that
+              <br className="hidden md:block" />
+              ruins friendships{" "}
+            </h1>
             <p className="mb-6 text-base text-indigo-100 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptate
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae.explicabo.Sed ut perspiciatis unde omnis.
+              Create or join a room, make your own avatar and start playing
+              <br></br>
+              with your friends in a fun and chaotic drawing game.
             </p>
 
             <div className="">
@@ -58,6 +58,362 @@ const App = () => {
                 <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z" />
               </svg>
             </a>
+          </div>
+        </div>
+      </div>
+      {/* Hey */}
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-16 lg:pt-48">
+        <div className="flex flex-col items-start max-w-screen-sm md:flex-row sm:mx-auto">
+          <a href="/" className="mb-4 mr-8">
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </a>
+          <div>
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-800 uppercase">
+              Cross-Platform
+            </p>
+            <h2 className="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Play anywhere you want
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              You can play <strong>Drawshift</strong> on mobile platforms (iOS,
+              Android) or in the browser and any touch devices.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*  */}
+      {/* <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-64">
+        <div className="max-w-screen-sm sm:text-center sm:mx-auto">
+          <a
+            href="/"
+            aria-label="View"
+            className="inline-block mb-5 rounded-full sm:mx-auto"
+          >
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </a>
+          <h2 className="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            Cross-platform
+          </h2>
+          <p className="text-base text-gray-700 md:text-lg sm:px-4">
+            You can play <strong>Drawshift</strong> on mobile devices or in the
+            browser
+          </p>
+          <hr className="w-full my-8 border-gray-300" />
+        </div>
+      </div> */}
+      {/* Hey */}
+      <div className="mx-auto lg:max-w-2xl">
+        <div className="relative w-full transition-shadow duration-300 hover:shadow-xl">
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg sm:h-64 md:h-80 lg:h-96"
+            src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt=""
+          />
+          <a
+            href="/"
+            aria-label="Play Video"
+            className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 group hover:bg-opacity-25"
+          >
+            <div className="flex items-center justify-center w-16 h-16 transition duration-300 transform bg-gray-100 rounded-full shadow-2xl group-hover:scale-110">
+              <svg
+                className="w-10 text-gray-900"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M16.53,11.152l-8-5C8.221,5.958,7.833,5.949,7.515,6.125C7.197,6.302,7,6.636,7,7v10 c0,0.364,0.197,0.698,0.515,0.875C7.667,17.958,7.833,18,8,18c0.184,0,0.368-0.051,0.53-0.152l8-5C16.822,12.665,17,12.345,17,12 S16.822,11.335,16.53,11.152z" />
+              </svg>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      {/* Hey */}
+
+      <div className="relative mt-64 bg-deep-purple-accent-400">
+        <svg
+          className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-accent-400"
+          preserveAspectRatio="none"
+          viewBox="0 0 1440 54"
+        >
+          <path
+            fill="currentColor"
+            d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
+          />
+        </svg>
+        <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+            <div className="md:max-w-md lg:col-span-2">
+              <a
+                href="/"
+                aria-label="Go home"
+                title="Company"
+                className="inline-flex items-center"
+              >
+                <svg
+                  className="w-8 text-teal-accent-400"
+                  viewBox="0 0 24 24"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  stroke="currentColor"
+                  fill="none"
+                >
+                  <rect x="3" y="1" width="7" height="12" />
+                  <rect x="3" y="17" width="7" height="6" />
+                  <rect x="14" y="1" width="7" height="6" />
+                  <rect x="14" y="11" width="7" height="12" />
+                </svg>
+                <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                  Company
+                </span>
+              </a>
+              <div className="mt-4 lg:max-w-sm">
+                <p className="text-sm text-deep-purple-50">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam.
+                </p>
+                <p className="mt-4 text-sm text-deep-purple-50">
+                  Eaque ipsa quae ab illo inventore veritatis et quasi
+                  architecto beatae vitae dicta sunt explicabo.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  Category
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      News
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      World
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Games
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      References
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  Cherry
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Web
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      eCommerce
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Business
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Entertainment
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Portfolio
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  Apples
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Media
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Brochure
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Nonprofit
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Educational
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Projects
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  Business
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Infopreneur
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Personal
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Wiki
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Forum
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+            <p className="text-sm text-gray-100">
+              © Copyright 2020 Lorem Inc. All rights reserved.
+            </p>
+            <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                  <circle cx="15" cy="15" r="4" />
+                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
