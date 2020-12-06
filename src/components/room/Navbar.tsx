e9b0f1081acd37da776e61aea992a623c4a709e1
Qalamar <contact@tariqhamrit.com>
2020-12-06 21:36:39 +0100

    Navbar component

--- src/components/room/Navbar.tsx
+++ src/components/room/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Logo from "./navbar/Logo";
+import Navigation from "./navbar/Navigation";
+import RoomCode from "./navbar/RoomCode";
 import { styles } from "./RoomStyles";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <div className="flex-none w-64 h-64 bg-vod-logo rounded-r-full border-r-8 border-vod-primary"></div>
-      <div className="flex-grow "></div>
-      <div className="flex-none w-64"></div>
+      <Logo />
+      <RoomCode />
+      <Navigation />
     </div>
   );
 };
---
+++ src/components/room/navbar/Navigation.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { avatar } from "../../../utils/Store";
+import { BigHead } from "@bigheads/core";
+
+const Navigation = () => {
+  return (
+    <div className="flex-none w-min flex flex-row px-6 invisible md:visible">
+      <div className="rounded-full text-vod-content bg-vod-action cursor-pointer font-semibold shadow-card flex justify-center items-center w-auto h-12 mx-3 px-4">
+        <div className="w-12 h-12 -mt-2">
+          <BigHead {...avatar} />
+        </div>
+        <span className="px-2">Xorium</span>
+      </div>
+      <div className="rounded-full text-vod-primary bg-vod-action cursor-pointer font-semibold shadow-card flex justify-center items-center w-12 h-12 mx-3">
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="2.9707"
+            width="20"
+            height="4"
+            rx="2"
+            transform="rotate(45 2.9707 0)"
+            fill="#4E4B66"
+          />
+          <rect
+            x="16.9707"
+            y="3"
+            width="20"
+            height="4"
+            rx="2"
+            transform="rotate(135 16.9707 3)"
+            fill="#4E4B66"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
