54b7dbe4366ab4d9cb308e23112b66e35da771bf
Tariq Hamrit <contact@tariqhamrit.com>
2021-02-07 07:45:26 +0100

    Updates

--- src/pages/boards/Boards.styled.tsx
+++ src/pages/boards/Boards.styled.tsx
@@ -1,11 +1,21 @@
 import tw, { styled } from "twin.macro";
 
+interface NavProps {
+  selected?: boolean;
+}
+
 export const Container = styled.div`
   ${tw`flex flex-row w-screen h-screen overflow-hidden`}
 `;
 
 export const SplitPane = styled.div`
-  ${tw`flex flex-col h-screen px-4 py-8 border-r w-72 bg-colors-lightgray`}
+  ${tw`flex flex-col h-screen px-4 border-r pt-14 w-72 bg-colors-lightgray`}
+`;
+
+export const NavItem = styled.a`
+  ${tw`flex items-center px-4 py-2 mt-3 text-lg text-gray-500 transition duration-200 rounded hover:bg-gray-200 hover:text-gray-700`}
+  ${({ selected }: NavProps) =>
+    selected && tw`text-gray-700 hover:bg-colors-gray bg-colors-gray`}
 `;
 
 export const Page = styled.div`
