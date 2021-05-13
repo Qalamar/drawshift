425acb82cbff3c51bea317a114c01b81ab212fe3
Tariq <contact@tariqhamrit.com>
2021-05-13 17:50:32 +0100

    Board calls

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition, Menu } from "@headlessui/react";
 import {
   ArchiveIcon,
   BanIcon,
@@ -11,6 +11,16 @@ import {
   UserCircleIcon,
   XIcon,
 } from "@heroicons/react/outline";
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  ChevronDownIcon,
+  CurrencyDollarIcon,
+  LocationMarkerIcon,
+  ReplyIcon,
+  SaveAsIcon,
+  TrashIcon,
+} from "@heroicons/react/solid";
 import Spinner from "components/Spinner";
 import { supabase } from "lib/initSupabase";
 import { useRouter } from "next/router";
@@ -19,6 +29,7 @@ import { useQuery } from "react-query";
 import RangeSlider from "react-bootstrap-range-slider";
 import { Main } from "./board.styled";
 import Header from "./Header";
+import { compress, decompress } from "lzutf8";
 import CanvasDraw from "react-canvas-draw";
 import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
 const views = [
@@ -89,6 +100,7 @@ export default function Example() {
     },
   });
 
+  const [userData, setuserData] = useState();
   const [brushColor, setbrushColor] = useState("#0e0e0e0");
   const [brushRadius, setbrushRadius] = useState(5);
   const updateColors = (color: string) => {
@@ -101,13 +113,25 @@ export default function Example() {
     supabase.auth.signOut();
     router.push("/");
   };
+
+  const saveBoard = async () => {
+    const { data, error } = await supabase.from("boards").insert([
+      {
+        user_id: userData.id,
+        title: "Title",
+        board: compress(saveableCanvas.getSaveData()),
+      },
+    ]);
+    console.log(data);
+  };
+
   useEffect(() => {
     console.log(user);
 
     supabase.auth.refreshSession();
     const userData = supabase.auth.user();
     console.log(userData);
-
+    setuserData(userData);
     fetchPath(userData);
   }, []);
   // @ts-ignore
@@ -121,11 +145,11 @@ export default function Example() {
     // console.log("error", error);
     console.log(userData);
 
-    const { data, error } = await supabase
-      .from("boards")
-      .insert([{ user_id: userData.id, board: "Hmmm" }]);
-    console.log("boards", data);
-    console.log("error", error);
+    // const { data, error } = await supabase
+    //   .from("boards")
+    //   .insert([{ user_id: userData.id, board: "Hmmm" }]);
+    // console.log("boards", data);
+    // console.log("error", error);
   };
   if (isLoading) return <Spinner />;
 
@@ -310,7 +334,145 @@ export default function Example() {
         {/* Main area */}
         <Main>
           {/* Page header */}
-          <Header canvas={saveableCanvas} />
+          <div className="mx-auto lg:flex lg:items-center lg:justify-between max-w-7xl">
+            <div className="flex-1 min-w-0">
+              <nav className="flex" aria-label="Breadcrumb"></nav>
+              <h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                Back End Developer
+              </h2>
+              <div className="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+                <div className="flex items-center mt-2 text-sm text-gray-500">
+                  <BriefcaseIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  Full-time
+                </div>
+                <div className="flex items-center mt-2 text-sm text-gray-500">
+                  <LocationMarkerIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  Remote
+                </div>
+                <div className="flex items-center mt-2 text-sm text-gray-500">
+                  <CurrencyDollarIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  $120k &ndash; $140k
+                </div>
+                <div className="flex items-center mt-2 text-sm text-gray-500">
+                  <CalendarIcon
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  Closing on January 9, 2020
+                </div>
+              </div>
+            </div>
+            <div className="flex mt-5 lg:mt-0 lg:ml-4">
+              <span className="hidden sm:block">
+                <button
+                  onClick={() => saveableCanvas.undo()}
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <ReplyIcon
+                    className="w-5 h-5 mr-2 -ml-1 text-gray-500"
+                    aria-hidden="true"
+                  />
+                  Undo
+                </button>
+              </span>
+
+              <span className="hidden ml-3 sm:block">
+                <button
+                  onClick={() => saveableCanvas.clear()}
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <TrashIcon
+                    className="w-5 h-5 mr-2 -ml-1 text-gray-500"
+                    aria-hidden="true"
+                  />
+                  Clear
+                </button>
+              </span>
+
+              <span className="sm:ml-3">
+                <button
+                  onClick={() => saveBoard()}
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <SaveAsIcon
+                    className="w-5 h-5 mr-2 -ml-1"
+                    aria-hidden="true"
+                  />
+                  Save
+                </button>
+              </span>
+
+              {/* Dropdown */}
+              <Menu as="span" className="relative ml-3 sm:hidden">
+                {({ open }) => (
+                  <>
+                    <Menu.Button className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      More
+                      <ChevronDownIcon
+                        className="w-5 h-5 ml-2 -mr-1 text-gray-500"
+                        aria-hidden="true"
+                      />
+                    </Menu.Button>
+
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items
+                        static
+                        className="absolute right-0 w-48 py-1 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      >
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Edit
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              View
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </>
+                )}
+              </Menu>
+            </div>
+          </div>
           <div className="h-auto mx-auto mt-8 bg-white rounded-lg shadow-lg max-w-7xl">
             <CanvasDraw
               brushColor={brushColor}
